2bb17d7de69054700e6a9af5ec6a0f7dca949b74
Ariff <ariffazlan16@gmail.com>
2023-06-24 23:22:08 +0800

    add functionality to signUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,11 +2,57 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+import { useState, useEffect, useMemo } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
 function SignUp() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+    const { name, email, password } = formData;
+
+    const navigate = useNavigate();
+
+    const users = useMemo(() => {
+        return JSON.parse(localStorage.getItem('users')) || [];
+    }, []);
+
+    useEffect(() => {
+        // Save the form data to local storage whenever it changes
+        localStorage.setItem('users', JSON.stringify(users));
+    }, [formData, users]);
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+        }));
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            if (name && email && password) {
+                const newUser = { name, email, password };
+                const updatedUsers = [...users, newUser];
+                localStorage.setItem('users', JSON.stringify(updatedUsers));
+                navigate('/sign-in');
+            } else {
+                throw new Error('Invalid registration details');
+            }
+        } catch (error) {
+            toast.error('Something went wrong with registration');
+        }
+    };
+
     return (
         <>
             <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-                <Card className='mx-auto' style={{ width: '30rem', height: '25rem' }}>
+                <Card className='mx-auto' style={{ width: '30rem', height: '30rem' }}>
                     <Card.Body>
                         <Card.Title className="text-center">Sign Up</Card.Title>
                         <Card.Text className="text-center">
@@ -14,12 +60,35 @@ function SignUp() {
                         </Card.Text>
                     </Card.Body>
                     <br />
-                    <div className="d-grid gap-3 mb-3 p-4">
-                        <Form.Control type="text" placeholder="Name" />
-                        <Form.Control type="text" placeholder="Email" />
-                        <Form.Control type="password" placeholder="Password" />
-                        <br />
-                        <Button variant="primary">Register</Button>{' '}
+                    <div>
+                        <Form className="d-grid gap-3 mb-3 p-4" onSubmit={onSubmit}>
+                            <Form.Control
+                                type="text"
+                                id="name"
+                                placeholder="Name"
+                                value={name}
+                                onChange={onChange}
+                            />
+                            <Form.Control
+                                type="email"
+                                id="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={onChange}
+                            />
+                            <Form.Control
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={onChange}
+                            />
+                            <br />
+                            <Button variant="primary" type="submit">
+                                Register
+                            </Button>{' '}
+                            <Link className="text-center mb-2" to="/sign-in">Already have an account? Sign in</Link>
+                        </Form>
                     </div>
                 </Card>
             </div>
